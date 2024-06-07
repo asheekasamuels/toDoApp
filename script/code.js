a3fbc1d89f6948afb22c466db77ca66429645a4f
asheekasamuels <samuelsasheeka133@gmail.com>
2024-06-07 11:01:32 +0200

    javaS

--- script/code.js
+++ script/code.js
@@ -1,1 +1,59 @@
-//
+let tasks = [];
+
+function adding() {
+  let taskList = {
+    name: document.getElementById("tasks").value,
+    completed: false
+  };
+  tasks.push(taskList);
+  let toDoList = document.querySelector("#results");
+  toDoList.innerHTML = "";
+  tasks.forEach((data) => {
+    toDoList.innerHTML += ` 
+      <input type="checkbox" onclick="toggleCompleted('${data.name}')">
+      <p>${data.name}</p>
+      <span class="close" onclick="removeTask('${data.name}')">x</span>
+    `;
+  });
+  stored();
+}
+
+function stored() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function dataRefresh() {
+  const storedTasks = localStorage.getItem("tasks");
+  if (storedTasks) {
+    tasks = JSON.parse(storedTasks);
+    const toDoList = document.querySelector("#results");
+    toDoList.innerHTML = "";
+    tasks.forEach((data) => {
+      toDoList.innerHTML += ` 
+        <input type="checkbox" onclick="toggleCompleted('${data.name}')">
+        <p>${data.name}</p>
+        <span class="close" onclick="removeTask('${data.name}')">x</span>
+      `;
+    });
+  }
+}
+
+function toggleCompleted(name) {
+  const task = tasks.find((t) => t.name === name);
+  if (task) {
+    task.completed = !task.completed;
+    stored();
+    dataRefresh();
+  }
+}
+
+function removeTask(name) {
+  tasks = tasks.filter((t) => t.name !== name);
+  stored();
+  dataRefresh();
+}
+
+function sorting() {
+  tasks.sort((a, b) => a.name.localeCompare(b.name));
+  dataRefresh();
+}
